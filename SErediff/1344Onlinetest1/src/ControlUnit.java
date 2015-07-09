00da61e51825b38d14581ee0dc09748598581bf8
Dell <Dell@Dell-PC>
2015-07-09 15:48:53 +0530

    commit1

---
+++ SErediff/1344Onlinetest1/src/ControlUnit.java
@@ -0,0 +1,14 @@
+
+public class ControlUnit {
+
+	DisplaySection ds=new DisplaySection();
+	CandidateSetSection css=new CandidateSetSection();
+	ResultSection rs=new ResultSection();
+	BallotSection bs=new BallotSection();
+	
+	
+	
+	
+	
+	
+}
