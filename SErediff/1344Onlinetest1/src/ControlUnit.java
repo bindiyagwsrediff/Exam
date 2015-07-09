b0669673b7834c2609f8222c8543d7fc1542753b
Dell <Dell@Dell-PC>
2015-07-09 16:37:34 +0530

    commit2

--- SErediff/1344Onlinetest1/src/ControlUnit.java
+++ SErediff/1344Onlinetest1/src/ControlUnit.java
@@ -1,12 +1,60 @@
 
-public class ControlUnit {
+public class ControlUnit implements EVM{
 
 	DisplaySection ds=new DisplaySection();
 	CandidateSetSection css=new CandidateSetSection();
 	ResultSection rs=new ResultSection();
 	BallotSection bs=new BallotSection();
 	
-	
+	@Override
+	public void on() {
+		ds.on();
+	}
+	@Override
+	public void busy() {
+		// TODO Auto-generated method stub
+		ds.busy();
+	}
+	@Override
+	public void dispPanel() {
+		ds.dispPanel();
+		
+	}
+	@Override
+	public void twoDigitDisplay() {
+		// TODO Auto-generated method stub
+		ds.twoDigitDisplay();
+	}
+	@Override
+	public void fourDigitDisplay() {
+		// TODO Auto-generated method stub
+		ds.fourDigitDisplay();
+	}
+	@Override
+	public void candidateSetButton() {
+		// TODO Auto-generated method stub
+		css.candidateSetButton();
+	}
+	@Override
+	public void closeButton() {
+		rs.closeButton();
+		
+	}
+	@Override
+	public void result1Button() {
+		// TODO Auto-generated method stub
+		rs.result1Button();
+	}
+	@Override
+	public void totalButton() {
+		bs.totalButton();
+		
+	}
+	@Override
+	public void ballotButton() {
+		// TODO Auto-generated method stub
+		bs.ballotButton();
+	}
 	
 	
 	
